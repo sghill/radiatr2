a4096bffc7caa5365974f7c17df81f66b1a66216
Tyler Mercier <tylermercier@gmail.com>
2011-05-02 23:03:26 -0700

    change util class over to object literal

--- js/bm/jenkins_item_renderer.js
+++ js/bm/jenkins_item_renderer.js
@@ -3,12 +3,11 @@ var BM = BM || {};
 BM.JenkinsItemRenderer = function (options) {
 	var template_id = options.template_id;
 	var placeholder_id = options.placeholder_id;
-	var utils = new BM.Utils();
 	
   return {
 		get : function(url) {
 			console.debug('BM.JenkinsItemRenderer get url:' + url);
-			utils.getJSONP(url, this.load);
+			BM.Utils.getJSONP(url, this.load);
 		},
 		load : function(data) {
 			console.debug('BM.JenkinsItemRenderer load data:' + data);
